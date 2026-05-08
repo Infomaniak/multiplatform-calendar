45b7ef5ca4dfa3a394f56e1b76e4a45ae9a1dccf
Vincent Te <vincent.te@infomaniak.com>
2026-05-08 12:40:21 +0200

    chore: Fix CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,21 @@
+/*
+ * Infomaniak Calendar - Multiplatform
+ * Copyright (C) 2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import co.touchlab.skie.configuration.DefaultArgumentInterop
 import com.android.build.api.dsl.androidLibrary
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
@@ -13,7 +31,7 @@ plugins {
 }
 
 val androidCompileSdk: Int = 36
-val androidMinSdk: Int  = 27
+val androidMinSdk: Int = 27
 val javaVersion: JavaVersion = JavaVersion.VERSION_17
 
 kotlin {

d9e276d0857112b3b8ef929eca9c0f3667be71e8
Benjamin Vadon <benjamin.vadon@infomaniak.com>
2026-05-12 15:55:25 +0200

    feat: Add event model

---
+++ src/commonMain/kotlin/com/infomaniak/multiplatform_calendar/model/event/Event.kt
@@ -0,0 +1,42 @@
+/*
+ * Infomaniak APPNAME - Android
+ * Copyright (C) 2026-2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.infomaniak.multiplatform_calendar.model.event
+
+import com.infomaniak.multiplatform_calendar.model.calendar.CalendarId
+import com.infomaniak.multiplatform_calendar.model.event.recurenceRule.RecurrenceRule
+import kotlin.time.Instant
+
+data class Event (
+    val id: EventId,
+    val calendarId: CalendarId,
+    val remoteUid: String,
+    val title: String,
+    val description: String? = null,
+    val location: String? = null,
+    val start: Instant,
+    val end: Instant,
+    val isAllDay: Boolean = false,
+    val recurrenceRule: RecurrenceRule? = null,
+    val etag: String? = null,
+    val rawIcs: String? = null,
+    val lastModified: Instant,
+    val isSynced: Boolean = false,
+    val attendees: List<Attendee> = emptyList(),
+    val organizer: Attendee? = null,
+)

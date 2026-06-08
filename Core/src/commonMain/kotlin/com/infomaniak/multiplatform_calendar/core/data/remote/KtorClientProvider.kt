bdd6396db3dfc1b0ae67864f7bd495eb075b21c3
Benjamin Vadon <benjamin.vadon@infomaniak.com>
2026-06-08 14:46:25 +0200

    feat: Add ktor client

---
+++ Core/src/commonMain/kotlin/com/infomaniak/multiplatform_calendar/core/data/remote/KtorClientProvider.kt
@@ -0,0 +1,45 @@
+/*
+ * Infomaniak Calendar - Multiplatform
+ * Copyright (C) 2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.multiplatform_calendar.core.data.remote
+
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.ContributesTo
+import dev.zacsweers.metro.Provides
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.http.URLProtocol
+import io.ktor.http.path
+import io.ktor.serialization.kotlinx.json.json
+
+@ContributesTo(AppScope::class)
+interface KtorClientProvider {
+    @Provides
+    fun provideHttpClient(): HttpClient = HttpClient {
+        install(ContentNegotiation) {
+            json()
+        }
+        defaultRequest {
+            url {
+                protocol = URLProtocol.HTTPS
+                host = "api.infomaniak.com"
+                path("1/")
+            }
+        }
+    }
+}

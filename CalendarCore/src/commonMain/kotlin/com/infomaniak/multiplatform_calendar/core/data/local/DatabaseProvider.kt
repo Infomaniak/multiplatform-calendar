09aec675a5089e9f08c31e9639c4566cf5e8fbf8
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2026-07-09 15:44:09 +0200

    chore: Restore 4.json and upgrade migration to 6

--- CalendarCore/src/commonMain/kotlin/com/infomaniak/multiplatform_calendar/core/data/local/DatabaseProvider.kt
+++ CalendarCore/src/commonMain/kotlin/com/infomaniak/multiplatform_calendar/core/data/local/DatabaseProvider.kt
@@ -50,7 +50,7 @@ internal fun DatabaseProvider.getCalendarDatabase(
 
 @Database(
     entities = [AccountEntity::class, CalendarEntity::class, EventEntity::class],
-    version = 5,
+    version = 6,
     exportSchema = true,
 )
 @TypeConverters(CalendarTypeConverters::class)

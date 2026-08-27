375674b3517830283af13a5e9b3f523ce5d78309
Benjamin Vadon <benjamin.vadon@infomaniak.com>
2026-08-27 14:06:19 +0200

    feat: Add an EXDATE/RDATE change to the event edit FFI

--- CalendarCore/src/commonMain/kotlin/com/infomaniak/multiplatform_calendar/core/data/mapper/EventEditMapper.kt
+++ CalendarCore/src/commonMain/kotlin/com/infomaniak/multiplatform_calendar/core/data/mapper/EventEditMapper.kt
@@ -31,6 +31,7 @@ import com.infomaniak.multiplatform_calendar.core.extensions.toICalDate
 import com.infomaniak.multiplatform_calendar.core.extensions.toICalLocalDateTime
 import com.infomaniak.multiplatform_calendar.core.extensions.toICalUtcDateTime
 import com.infomaniak.multiplatform_calendar.data.remote.caldav.model.RemoteColorChange
+import com.infomaniak.multiplatform_calendar.data.remote.caldav.model.RemoteDateListChange
 import com.infomaniak.multiplatform_calendar.data.remote.caldav.model.RemoteEventEdit
 import com.infomaniak.multiplatform_calendar.data.remote.caldav.model.RemoteRecurrenceChange
 import com.infomaniak.multiplatform_calendar.data.remote.caldav.model.RemoteRecurrenceChange.Cleared
@@ -64,6 +65,8 @@ internal fun EventEditData.toRemoteEdit(stamp: String, previous: EventEntity?): 
         timeZones = timing.vTimeZones(),
         colorChange = resolveColorChange(previous?.content?.colorArgb),
         recurrenceChange = resolveRecurrenceChange(previous?.rrule),
+        exDateChange = RemoteDateListChange.Unchanged,
+        rDateChange = RemoteDateListChange.Unchanged,
         alarms = resolveAlarmEdits(alarms, previous?.content?.alarms.orEmpty()),
         stamp = stamp,
     )
